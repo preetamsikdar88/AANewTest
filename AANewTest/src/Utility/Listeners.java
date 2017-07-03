6e5a2fff502dca64bc70f7f5ae226e6e677e67e7
preetamsikdar88 <preetamsikdar30@gmail.com>
2017-07-03 16:14:52 +0530

    Commiting Listener class

---
+++ AANewTest/src/Utility/Listeners.java
@@ -0,0 +1,65 @@
+package Utility;
+
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listeners implements ITestListener, IInvokedMethodListener{
+
+	@Override
+	public void onFinish(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailure(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestStart(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult arg0) {
+		System.out.println("Test Case passed");
+		
+	}
+
+	@Override
+	public void afterInvocation(IInvokedMethod arg0, ITestResult arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeInvocation(IInvokedMethod arg0, ITestResult arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
